ec0a9ee96863d5c5c798e2104da77e7007219001
Anshul Jain <anshuljain21120@gmail.com>
2020-07-15 14:31:53 +0530

    input valid first name

--- userRegistration.sh
+++ userRegistration.sh
@@ -2,3 +2,15 @@
 
 echo "Welcome to the User Registration problem!";
 echo "A user registration System needs to ensure all validations are in place during the User Entry";
+
+isValid=0;
+while [ $isValid -eq 0 ]
+do
+	read -p "Enter valid first name: " first_name;
+	if [[ $first_name =~ ^[A-Z][A-Za-z]{2,}$ ]]
+	then
+		isValid=1;
+	else
+		echo "Invalid Input! Enter again.";
+	fi
+done
